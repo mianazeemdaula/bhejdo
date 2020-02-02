f435361209362142494c57d0045a8aba9ad71a5f
mianazeemdaula <mianazeemdaula@gmail.com>
2020-02-02 14:08:27 +0500

    transaction

--- app/Http/Controllers/Api/Lifter/MilkOrderController.php
+++ app/Http/Controllers/Api/Lifter/MilkOrderController.php
@@ -12,6 +12,7 @@ use App\OrderDetail;
 use Illuminate\Support\Facades\Auth;
 use Validator;
 use App\Helpers\AndroidNotifications;
+use DB;
 
 class MilkOrderController extends Controller
 {
@@ -30,19 +31,23 @@ class MilkOrderController extends Controller
     public function updateOrder(Request $request)
     {
         try{
-            $order = Order::findOrFail($request->order_id);
-            $order->status = $request->status;
-            $order->save();
-            $order->details()->insert([
-                'lifter_id' => $request->user()->id,
-                'order_id' => $request->order_id,
-                'status' => $request->status,
-            ]);
-            $message = 'You order is '.$request->status;
-            $notification = AndroidNotifications::toConsumer($request->user()->name, $message, $order->consumer->pushToken,[]);
-            $data = [ 'message' => "Order $request->status sucessfully", 'notification' => $notification];
+            DB::transaction(function () {
+            
+                $order = Order::findOrFail($request->order_id);
+                $order->status = $request->status;
+                $order->save();
+                $order->delivery()->insert([
+                    'lifter_id' => $request->user()->id,
+                    'order_id' => $request->order_id,
+                    'status' => $request->status,
+                ]);
+                $message = 'You order is '.$request->status;
+                $notification = AndroidNotifications::toConsumer($request->user()->name, $message, $order->consumer->pushToken,[]);
+                $data = [ 'message' => "Order $request->status sucessfully", 'notification' => $notification];
+            }, 2);
             return response()->json(['status'=>true, 'data' => $data], 200);
         }catch(Exception $ex){
+            DB::rollBack();
             return response()->json(['status'=>false, 'data'=>"$ex"], 401);
         }
     }
