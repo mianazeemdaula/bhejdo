1b1d5cdd0cd2530b16b4727e2988f26d2530d998
mianazeemdaula <mianazeemdaula@gmail.com>
2020-02-10 12:11:46 +0500

    as

--- app/Http/Controllers/Api/Lifter/EventController.php
+++ app/Http/Controllers/Api/Lifter/EventController.php
@@ -66,6 +66,7 @@ class EventController extends Controller
                                 ]
                             ]
                         ]
+                    ]
                 ]
             ]
         ];
