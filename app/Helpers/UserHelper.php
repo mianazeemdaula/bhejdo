e5377cbc0ec4d7e73bb856fec93a0a3b82a62706
mianazeemdaula <mianazeemdaula@gmail.com>
2020-02-18 10:55:22 +0500

    id

--- app/Helpers/UserHelper.php
+++ app/Helpers/UserHelper.php
@@ -8,7 +8,7 @@ use App\User;
 class UserHelper{
     static public function gerateId($username){
         $refferid = Str::limit($username,5);
-        $newid = $newid.Str::random(10 - strlen($refferid));
+        $newid = $refferid.Str::random(10 - strlen($refferid));
         $isAvaialbe = User::where('reffer_id', $newid)->first();
         if($isAvaialbe == null){
             return $newid;
