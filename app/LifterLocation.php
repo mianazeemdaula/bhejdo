d2cf3d05d0de4f3dc016b74ff0f5ac5488687511
mianazeemdaula <mianazeemdaula@gmail.com>
2020-02-18 22:54:32 +0500

    fillable

--- app/LifterLocation.php
+++ app/LifterLocation.php
@@ -9,4 +9,7 @@ class LifterLocation extends Model
     protected $collection = 'lifter_location';
     protected $connection = 'mongodb';
     protected $primaryKey = 'id';
+    protected $fillable = [
+        'address',
+    ];
 }
