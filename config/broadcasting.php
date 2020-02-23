748925c00e70b6be8778f4676dbf44496651077d
mianazeemdaula <mianazeemdaula@gmail.com>
2020-02-23 16:55:58 +0500

    broadcast

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -42,10 +42,10 @@ return [
                 'host' => '127.0.0.1',
                 'port' => 6001,
                 'scheme' => 'https',
-                // 'curl_options' => [
-                //     CURLOPT_SSL_VERIFYHOST => 0,
-                //     CURLOPT_SSL_VERIFYPEER => 0,
-                // ]
+                'curl_options' => [
+                    CURLOPT_SSL_VERIFYHOST => 0,
+                    CURLOPT_SSL_VERIFYPEER => 0,
+                ]
             ],
         ],
 
