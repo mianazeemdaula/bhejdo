9380637c99258c4ff2e622ff16f63468c99edffc
mianazeemdaula <mianazeemdaula@gmail.com>
2020-02-23 14:52:34 +0500

    broadscas

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -41,7 +41,11 @@ return [
                 // 'encrypted' => true,
                 'host' => '127.0.0.1',
                 'port' => 6001,
-                'scheme' => 'https'
+                'scheme' => 'https',
+                'curl_options' => [
+                    CURLOPT_SSL_VERIFYHOST => 0,
+                    CURLOPT_SSL_VERIFYPEER => 0,
+                ]
             ],
         ],
 
