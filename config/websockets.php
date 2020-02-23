4799b6250323f202cba01af5ba6581e4002a6468
mianazeemdaula <mianazeemdaula@gmail.com>
2020-02-23 14:54:25 +0500

    asdf

--- config/websockets.php
+++ config/websockets.php
@@ -110,13 +110,13 @@ return [
          * certificate chain of issuers. The private key also may be contained
          * in a separate file specified by local_pk.
          */
-        'local_cert' => "/etc/letsencrypt/live/bhejdo.org/privatekey.pem",
+        'local_cert' => "/etc/letsencrypt/live/bhejdo.org/fullchain.pem",
 
         /*
          * Path to local private key file on filesystem in case of separate files for
          * certificate (local_cert) and private key.
          */
-        'local_pk' => "/etc/letsencrypt/live/bhejdo.org/fullchain.pem",
+        'local_pk' => "/etc/letsencrypt/live/bhejdo.org/privatekey.pem",
 
         /*
          * Passphrase for your local_cert file.
