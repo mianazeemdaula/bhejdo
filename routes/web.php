7dae12581f9743a5567cce9961f7e96e7c903519
mianazeemdaula <mianazeemdaula@gmail.com>
2020-02-26 20:57:56 +0500

    services

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Redis;
 
 Route::get('/redis', function () {
     $app = Redis::connection();
-    $app->set('user', App\User::with('service')->get()->toJson());
+    $app->set('user', App\User::with('services')->get()->toJson());
     return $app->get('user');
 });
 
