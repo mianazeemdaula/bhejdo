b0b1a9af77fcdfd463060a7a7ba666fda420f620
mianazeemdaula <mianazeemdaula@gmail.com>
2020-02-26 20:42:28 +0500

    redis

--- config/database.php
+++ config/database.php
@@ -131,7 +131,7 @@ return [
 
     'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'phpredis'),
+        'client' => env('REDIS_CLIENT', 'predis'),
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'predis'),
@@ -139,16 +139,16 @@ return [
         ],
 
         'default' => [
-            //'url' => env('REDIS_URL'),
+            'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
-            'read_timeout' => 60,
+            //'read_timeout' => 60,
         ],
 
         'cache' => [
-            //'url' => env('REDIS_URL'),
+            'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', '6379'),
