de8f9213ce68db28949cc6f6b6af230a3e5c68d3
mianazeemdaula <mianazeemdaula@gmail.com>
2020-03-04 14:29:41 +0500

    description

--- app/ServiceCharge.php
+++ app/ServiceCharge.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class ServiceCharge extends Model
 {
     protected $fillable = [
-        'user_id', 'type', 'description', 'amount'
+        'user_id', 'type', 'description', 'amount', 'balance'
     ];
 }
