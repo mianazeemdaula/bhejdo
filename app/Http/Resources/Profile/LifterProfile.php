fca2bcbcf18d232e6498cdf0c624c1dc8f21b6fe
mianazeemdaula <mianazeemdaula@gmail.com>
2020-03-14 22:17:23 +0500

    profile

--- app/Http/Resources/Profile/LifterProfile.php
+++ app/Http/Resources/Profile/LifterProfile.php
@@ -14,6 +14,7 @@ class LifterProfile extends JsonResource
      */
     public function toArray($request)
     {
+        $rating = \App\Order::leftJoin('reviews','reviews.order_id','=','orders.id')->where('orders.lifter_id',$this->id)->where('reviews.type','lifter')->avg('reviews.starts');
         return [
             'id' => $this->id,
             'name' => $this->name,
@@ -27,7 +28,7 @@ class LifterProfile extends JsonResource
             'services' => ProfileService::collection($this->services),
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
-            'rating' => \App\Order::leftJoin('reviews','reviews.order_id','=','orders.id')->where('orders.lifter_id',$this->id)->where('reviews.type','lifter')->avg('reviews.starts')
+            'rating' => $rating == null ? 0.0 : $rating
         ];
     }
 }
