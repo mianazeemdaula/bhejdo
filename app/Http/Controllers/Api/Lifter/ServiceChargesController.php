f119e27859daa1502e01b16ab8b87fb255ae67d4
mianazeemdaula <mianazeemdaula@gmail.com>
2020-03-16 07:55:05 +0500

    charges

--- app/Http/Controllers/Api/Lifter/ServiceChargesController.php
+++ app/Http/Controllers/Api/Lifter/ServiceChargesController.php
@@ -9,6 +9,18 @@ use App\ServiceCharge;
 
 class ServiceChargesController extends Controller
 {
+    public function index(Request $request)
+    {
+        try{
+            $charges = ServiceCharge::where('user_id',$request->user()->id)->latest('id')->limit(100)->get();
+            $charges = \App\Http\Resources\LedgerEntry::collection($charges);
+            return response()->json(['status'=>true, 'data' => $charges], 200);
+        }catch(Exception $ex){
+            return response()->json(['status'=>false, 'data'=>"$ex"], 401);
+        }
+
+    }
+
     public function balance(Request $request)
     {
         try{
