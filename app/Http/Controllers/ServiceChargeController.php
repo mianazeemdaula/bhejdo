079d6a5dbf359fd1abf7abd8fcb10fc90dcfb15d
mianazeemdaula <mianazeemdaula@gmail.com>
2020-03-16 09:17:01 +0500

    serviceCharges

--- app/Http/Controllers/ServiceChargeController.php
+++ app/Http/Controllers/ServiceChargeController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class ServiceChargeController extends Controller
 {
-    //
+    public function index()
+    {
+        $services = Service::all();
+        return view('pages.admin.services.index', compact('services'));
+    }
 }
--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class WalletController extends Controller
 {
-    //
+    public function index()
+    {
+        $services = Service::all();
+        return view('pages.admin.services.index', compact('services'));
+    }
 }
