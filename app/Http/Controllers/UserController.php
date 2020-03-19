51704be407747d27080bd3deb8caac21ffc7e734
mianazeemdaula <mianazeemdaula@gmail.com>
2020-03-19 14:20:19 +0500

    role

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -34,7 +34,7 @@ class UserController extends Controller
 
     public function lifter()
     {
-        $users = User::role('lifter|store')->get();
+        $users = User::role('lifter')->role('store')->get();
         return view('pages.super-admin.user.index', compact('users'));
     }
 
