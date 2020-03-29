b303a9e36d6accbcdf035768bcee7a42479f38b6
mianazeemdaula <mianazeemdaula@gmail.com>
2020-03-29 14:08:46 +0500

    amount

--- resources/views/pages/admin/order/show.blade.php
+++ resources/views/pages/admin/order/show.blade.php
@@ -21,31 +21,26 @@
                   <div class="col-sm-4 invoice-col">
                     From
                     <address>
-                      <strong>Admin, Inc.</strong><br>
-                      795 Folsom Ave, Suite 600<br>
-                      San Francisco, CA 94107<br>
-                      Phone: (804) 123-5432<br>
-                      Email: info@almasaeedstudio.com
+                      <strong>{{ $order->consumer->name }}</strong><br>
+                      Phone: {{ $order->consumer->mobile }}<br>
+                      Email: {{ $order->consumer->email }}
                     </address>
                   </div>
                   <!-- /.col -->
                   <div class="col-sm-4 invoice-col">
                     To
                     <address>
-                      <strong>John Doe</strong><br>
-                      795 Folsom Ave, Suite 600<br>
-                      San Francisco, CA 94107<br>
-                      Phone: (555) 539-1037<br>
-                      Email: john.doe@example.com
-                    </address>
+                        <strong>{{ $order->lifter->name }}</strong><br>
+                        Phone: {{ $order->lifter->mobile }}<br>
+                        Email: {{ $order->lifter->email }}
+                      </address>
                   </div>
                   <!-- /.col -->
                   <div class="col-sm-4 invoice-col">
                     <b>Invoice #007612</b><br>
                     <br>
-                    <b>Order ID:</b> 4F3S8J<br>
-                    <b>Payment Due:</b> 2/22/2014<br>
-                    <b>Account:</b> 968-34567
+                    <b>Order ID:</b> {{ $order->id }}<br>
+                    <b>Payment:</b> {{ $order->payable_amount }}<br>
                   </div>
                   <!-- /.col -->
                 </div>
