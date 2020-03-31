e1021ed0d13173e8d868f9f6b24768ac503da136
mianazeemdaula <mianazeemdaula@gmail.com>
2020-03-31 21:37:51 +0500

    online

--- resources/views/pages/admin/lifters/index.blade.php
+++ resources/views/pages/admin/lifters/index.blade.php
@@ -35,7 +35,7 @@
                                     <td> {{ $item->name}} </td>
                                     <td> {{ $item->account_type}} </td>
                                     <td> {{ $item->last_update}} </td>
-                                    <td> {!! $item->location !!} </td>
+                                    <td> {!! $item->location['coordinates'][0] !!} </td>
                                 </tr>
                             @endforeach
                         </tbody>
