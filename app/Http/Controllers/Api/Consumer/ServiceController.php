c5b2fb9e6b61187d3b2da71460d606d7f70825c0
mianazeemdaula <mianazeemdaula@gmail.com>
2020-03-31 14:26:32 +0500

    services

--- app/Http/Controllers/Api/Consumer/ServiceController.php
+++ app/Http/Controllers/Api/Consumer/ServiceController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use App\Service;
+use App\Http\Resources\ServiceView;
 
 class ServiceController extends Controller
 {
@@ -13,6 +14,7 @@ class ServiceController extends Controller
     {
         try{
             $services = Service::where('s_status', 'active')->get();
+            $services = ServiceView::collection($services);
             return response()->json(['status'=>true, 'data' => $services], 200);
         }catch(Exception $ex){
             return response()->json(['status'=>false, 'data'=>"$ex"], 401);
--- app/Http/Resources/ServiceView.php
+++ app/Http/Resources/ServiceView.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class Service extends JsonResource
+class ServiceView extends JsonResource
 {
     /**
      * Transform the resource into an array.
