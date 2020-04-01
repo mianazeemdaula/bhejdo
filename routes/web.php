eba57ab62f0e9d7fe504f66f473aa550ee5fd374
mianazeemdaula <mianazeemdaula@gmail.com>
2020-04-01 15:24:20 +0500

    account_type

--- routes/web.php
+++ routes/web.php
@@ -113,19 +113,20 @@ Route::get('created_order', function(){
         return $orders;
 });
 
-Route::get('changeRole', function(){
-    $user = \App\User::find(22);
-    $user->account_type = 'lifter';
-    $user->removeRole('store');
-    $user->assignRole('store');
-    $user->save();
+// Route::get('changeRole', function(){
+//     $user = \App\User::find(22);
+//     $user->account_type = 'lifter';
+//     $user->removeRole('store');
+//     $user->assignRole('store');
+//     $user->save();
 
-    $user = \App\User::find(24);
-    $user->account_type = 'lifter';
-    $user->removeRole('store');
-    $user->assignRole('store');
-    $user->save();
-});
+//     $user = \App\User::find(24);
+//     $user->account_type = 'lifter';
+//     $user->removeRole('store');
+//     $user->assignRole('store');
+//     $user->save();
+// });
+
 Route::get('created_order', function(){
 
     $sOrder = \App\ScheduleOrder::find(1);
