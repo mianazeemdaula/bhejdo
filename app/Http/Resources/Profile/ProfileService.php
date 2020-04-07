5cc7dfe42ca45966514b2d946c5e2d93980aad9f
mianazeemdaula <mianazeemdaula@gmail.com>
2020-04-07 13:41:06 +0500

    authId

--- app/Http/Resources/Profile/ProfileService.php
+++ app/Http/Resources/Profile/ProfileService.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources\Profile;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Auth;
 
 class ProfileService extends JsonResource
 {
@@ -14,15 +15,15 @@ class ProfileService extends JsonResource
      */
     public function toArray($request)
     {
-        $ids = $this->orders()->where('status','confirmed')->orWhere('status','collected')->pluck('id')->toArray();
+        $ids = $this->orders()->where('lifter_id',Auth::id())->where('status','confirmed')->orWhere('status','collected')->pluck('id')->toArray();
         $rating = \App\Review::whereIn('order_id', $ids)->where('type', 'lifter')->avg('starts');
         return [
             'id' => $this->id,
             'name' => $this->s_name,
             'stars' => $rating == null ? 0 : (double) number_format($rating, 1),
             'orders' =>  count($ids),
-            'total' => $this->orders()->count(),
-            'deliver' => $this->orders()->where('status','delivered')->count(),
+            'total' => $this->orders()->where('lifter_id',Auth::id())->count(),
+            'deliver' => $this->orders()->where('lifter_id',Auth::id())->where('status','delivered')->count(),
         ];
     }
 }
