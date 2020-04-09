db5d3470bfed9cd1abdcb5f1affa5e415d1d874c
mianazeemdaula <mianazeemdaula@gmail.com>
2020-04-09 19:13:17 +0500

    shakal

--- routes/web.php
+++ routes/web.php
@@ -247,6 +247,5 @@ Route::get('/locations', function(){
 Route::get('queue', function(){
     $order = \App\Order::find(75);
     $data = \App\Helpers\OrderProcess::orderAssign($order);
-    return $order;
     return $data;
 });
