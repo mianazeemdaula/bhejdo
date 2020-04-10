12e3da0faa392500efd156c59357456612829e90
mianazeemdaula <mianazeemdaula@gmail.com>
2020-04-10 09:08:50 +0500

    sucess

--- routes/web.php
+++ routes/web.php
@@ -247,8 +247,8 @@ Route::get('queue', function(){
     $order = \App\Order::find(75);
     $user = \App\User::find(6);
     $notification = \App\Helpers\AndroidNotifications::toLifter("New Order","Just Test", $user->pushToken, []);
-    
-    return $notification;
+
+    return $notification['success'];
     $data = \App\Helpers\OrderProcess::orderAssign($order);
     return $data;
 });
