2228f33fea4b4f80ad4f96e04ad7950e9ead5b26
mianazeemdaula <mianazeemdaula@gmail.com>
2020-04-15 09:09:08 +0500

    lesthen

--- routes/web.php
+++ routes/web.php
@@ -229,5 +229,5 @@ Route::get('whereTime', function(){
         $time2->modify("+2 hour");
         $time2->modify('-'.$minutes2.' minutes');
     }
-    return \App\ScheduleOrder::where('delivery_time','>=', $time->format('H:i:s'))->where('delivery_time','<=', $time2->format('H:i:s'))->get();
+    return \App\ScheduleOrder::where('delivery_time','>=', $time->format('H:i:s'))->where('delivery_time','<', $time2->format('H:i:s'))->get();
 });
