1fc7de911a731a0ef1bf08e9e8964c774868901e
mianazeemdaula <mianazeemdaula@gmail.com>
2020-04-16 12:59:40 +0500

    fetchAccount

--- routes/api.php
+++ routes/api.php
@@ -61,8 +61,9 @@ Route::prefix('consumer')->group(function () {
         });
 
         Route::prefix('wallet')->group(function () {
-            Route::resource('/', 'Api\Consumer\WalletController');
-            Route::get('/balance', 'Api\Consumer\WalletController@balance');
+            Route::resource('/', 'Api\Lifter\WalletController');
+            Route::get('/balance', 'Api\Lifter\WalletController@balance');
+            Route::post('/fetchaccount', 'Api\Lifter\WalletController@fetchaccount');
         });
 
         Route::prefix('review')->group(function () {
