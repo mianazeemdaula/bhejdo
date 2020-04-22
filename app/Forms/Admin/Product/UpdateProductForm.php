d30624077a1090f4b742fd0ae838075b66c8bbf0
mianazeemdaula <mianazeemdaula@gmail.com>
2020-04-22 14:05:14 +0500

    status

--- app/Forms/Admin/Product/UpdateProductForm.php
+++ app/Forms/Admin/Product/UpdateProductForm.php
@@ -19,16 +19,34 @@ class UpdateProductForm extends Form
             'label' => 'Urdu Name'
         ])->add('contract_price', 'number',  [
             'rules' => 'required|min:1',
-            'label' => 'Contract Price' 
+            'label' => 'Contract Price (Company)' 
+        ])->add('min_qty_charges', 'number',  [
+            'rules' => 'required|min:1',
+            'label' => 'Delivery Charges on min qty' 
         ])->add('markeet_price', 'number',  [
             'rules' => 'required|min:1',
-            'label' => 'Markeet Price'
-        ])->add('weight', 'number',  [
+            'label' => 'Markeet Price (Company)'
+        ])->add('sale_price', 'number',  [
+            'rules' => 'required|min:1',
+            'label' => 'Sale Price'
+        ])->add('store_commission', 'number',  [
+            'rules' => 'required|min:1',
+            'label' => 'Store Comission'
+        ])
+        ->add('lifter_commission', 'number',  [
+            'rules' => 'required|min:1',
+            'label' => 'Lifter Comission'
+        ])
+        ->add('weight', 'number',  [
             'rules' => 'required|min:1',
             'label' => 'Weight (in grams)'
         ])->add('unit', 'text',  [
             'rules' => 'required|max:3',
             'label' => 'Unit'
+        ])->add('status', 'choice', [
+            'choices' => ['0' => 'Active', '1' => 'Deactive'],
+            'expanded' => true,
+            'multiple' => false
         ])->add('image','file',[
             'attr' =>[
                 'accept'=>"image/jpeg, image/png, image/jpg",
