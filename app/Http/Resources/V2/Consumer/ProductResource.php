37bc0a34ef0626008e26b1be7131051c5e8cf00b
mianazeemdaula <mianazeemdaula@gmail.com>
2020-04-22 18:12:28 +0500

    id

--- app/Http/Resources/V2/Consumer/ProductResource.php
+++ app/Http/Resources/V2/Consumer/ProductResource.php
@@ -18,7 +18,7 @@ class ProductResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'urduname' => $this->urdu_name,
-            'category' => $this->category->name,
+            'category' => [$this->category->id, $this->category->name],
             'company' => $this->company->name
         ];
     }
