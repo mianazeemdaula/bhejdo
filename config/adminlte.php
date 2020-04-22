3d8e8c122577369e5ea56dff8893609bde66f795
mianazeemdaula <mianazeemdaula@gmail.com>
2020-04-22 12:56:42 +0500

    ohYes

--- config/adminlte.php
+++ config/adminlte.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'title' => 'BhaijDo',
+    'title' => 'OhYes',
     'title_prefix' => '',
     'title_postfix' => '',
 
@@ -30,7 +30,7 @@ return [
     |
     */
 
-    'logo' => '<b>Bhaij</b>DO',
+    'logo' => '<b>OhYes</b>DO',
     'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
     'logo_img_class' => 'brand-image-xl',
     'logo_img_xl' => null,
