0b189a38148da04c278ee0600a1559acdd19b716
mianazeemdaula <mianazeemdaula@gmail.com>
2020-04-27 10:57:04 +0500

    getLat

--- app/Http/Resources/V2/Consumer/NearStoreResource.php
+++ app/Http/Resources/V2/Consumer/NearStoreResource.php
@@ -17,7 +17,7 @@ class NearStoreResource extends JsonResource
         return [
             'id' => $this->user_id,
             'name' => $this->user->name,
-            'location' => $this->location,
+            'location' => [$this->location->getLat(), $this->location->getLon()],
             'radius' => $this->radius,
         ];
     }
