6cf3cf1efe5381484db5171f4634b78ed6dbfc45
mianazeemdaula <mianazeemdaula@gmail.com>
2020-04-27 14:30:08 +0500

    bug

--- config/ohyes.php
+++ config/ohyes.php
@@ -2,10 +2,10 @@
 
 return [
     'consumer' => [
-        'bullet_service_charges' => 20,
+        'bullet_service_charges' => 50,
         'bullet_service_charges_cross' => 100,
-        'normal_delivery_fee' => 10,
+        'normal_delivery_fee' => 20,
         'normal_delivery_fee_cross' => 50,
-        'delivery_free_shopping' => 1000,
+        'delivery_free_shopping' => 400,
     ]
 ];
