663ede13e7a0bc80a4780b9b98bfc3fe05a55fbe
mianazeemdaula <mianazeemdaula@gmail.com>
2020-04-30 13:06:13 +0500

    users

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 use Kris\LaravelFormBuilder\FormBuilderTrait;
 use App\Forms\Admin\Sms\CreateSmsForm;
-
+use App\User;
 class SmsController extends Controller
 {
     use FormBuilderTrait;
