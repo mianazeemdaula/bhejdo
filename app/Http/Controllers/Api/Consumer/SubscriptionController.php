d7268bb90629bc2f01ca4f38d09516c540bd16b5
mianazeemdaula <mianazeemdaula@gmail.com>
2020-05-04 12:05:55 +0500

    subscription

---
+++ app/Http/Controllers/Api/Consumer/SubscriptionController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Api\Consumer;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use DB;
+use App\Subscription;
+
+class SubscriptionController extends Controller
+{
+    public function index(Request $request)
+    {
+        try{
+            $orders = Subscription::join('cart_orders', function($join)
+            {
+                $join->on('subscription.order_id', '=', 'cart_orders.consumer_id')
+                ->where('cart_orders.consumer_id',  $request->user()->id);
+            })->get();
+            //$orders = SubscriptionResource::collection($orders);
+            return response()->json(['status'=>true, 'data' => ['orders' => $orders ]], 200);
+        }catch(Exception $ex){
+            return response()->json(['status'=>false, 'data'=>"$ex"], 401);
+        }
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            DB::beginTransaction();
+            $deliveryTime = strlen($request->deliveryTime) > 2 ? $request->deliveryTime :  "0".$request->deliveryTime;
+            $subscription = new Subscription();
+            $subscription->order_id = $request->order;
+            $subscription->shift = $request->shift;
+            $subscription->delivery_time = $deliveryTime.":00:00";
+            $subscription->subscribe_type = $request->subscribe_type;
+            if($request->subscribe_type == 'daily'){
+                $subscription->days = [];
+            }else if($request->subscribe_type == 'weekdays'){
+                $subscription->days = json_decode($request->weekDays);
+            }else if($request->subscribe_type == 'monthly'){
+                $subscription->days = json_decode($request->monthDays);
+            }
+            $subscription->status = 1;
+            $subscription->save();
+            DB::commit();
+            return response()->json(['status'=>true, 'data' => 'Subscription added successfullly!'], 200);
+        }catch(Exception $e){
+            DB::rollBack();
+            return response()->json(['status'=>false, 'error' => "$e" ], 405);
+        }
+    }
+}
---
+++ database/migrations/2020_05_04_114257_create_subscriptions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubscriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('order_id');
+            $table->integer('shift');
+            $table->time('delivery_time');
+            $table->string('subscribe_type', 10);
+            $table->json('days');
+            $table->boolean('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+}
