9dd625d84c4c67f2adbfdbc628122f50a659376e
mianazeemdaula <mianazeemdaula@gmail.com>
2020-05-04 12:07:20 +0500

    subscription

--- routes/api.php
+++ routes/api.php
@@ -27,6 +27,7 @@ Route::prefix('consumer')->group(function () {
                 Route::resource('stores','StoresController');
                 Route::resource('address','AddressController');
                 Route::resource('order','CartOrderController');
+                Route::resource('subscription','SubscriptionController');
                 Route::resource('wallet', 'WalletController');
                 Route::resource('bonus', 'BonusController');
                 Route::get('profile','AuthController@profile');
