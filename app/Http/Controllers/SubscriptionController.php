f72cbd2c409a0fce5988f2db3582dbcf3c88499f
mianazeemdaula <mianazeemdaula@gmail.com>
2020-05-05 11:40:56 +0500

    subscriptin

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -4,7 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Subscription;
+
 class SubscriptionController extends Controller
 {
-    //
+    public function index()
+    {
+        $collection = Subscription::latest('id')->get();
+        return view('pages.admin.subscription.index', compact('collection'));
+    }
 }
