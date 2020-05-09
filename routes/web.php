dd2f180035cfa6ad6ba957329bc0a056ce91f818
mianazeemdaula <mianazeemdaula@gmail.com>
2020-05-09 09:44:30 +0500

    get

--- routes/web.php
+++ routes/web.php
@@ -116,5 +116,5 @@ Route::get('/pages/terms', function(){
 
 Route::get('catProduct', function(){
     $cat = \App\Category::find(2);
-    return $cat->cityProducts(1);
+    return $cat->cityProducts(1)->get();
 });
