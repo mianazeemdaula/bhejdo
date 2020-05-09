e6c1aea96b1baeef26d4e873f0d6e58562cba3ed
mianazeemdaula <mianazeemdaula@gmail.com>
2020-05-09 09:37:55 +0500

    cat

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    public function products()
+    {
+        return $this->hasMany('App\Product');
+    }
+
+    public function cityProducts($city)
+    {
+        return $this->hasMany('App\Product')->where('city_id',$city);
+    }
 }
