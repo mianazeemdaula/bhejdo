b878452dac632b606f8c381590547626899b25db
mianazeemdaula <mianazeemdaula@gmail.com>
2020-05-10 19:35:20 +0500

    freeDelivery

--- config/ohyes.php
+++ config/ohyes.php
@@ -6,7 +6,7 @@ return [
         'bullet_service_charges_cross' => 200,
         'normal_delivery_fee' => 100,
         'normal_delivery_fee_cross' => 150,
-        'delivery_free_shopping' => 1000,
+        'delivery_free_shopping' => 1500,
         'bullet_delivery_tme' => 90, // minutes
         'normal_delivery_tme' => 240, // minutes
     ]
