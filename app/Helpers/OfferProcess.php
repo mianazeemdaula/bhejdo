cd60684e51719477abc87b8520b69ca7c48355ee
mianazeemdaula <mianazeemdaula@gmail.com>
2020-05-14 10:02:17 +0500

    request

--- app/Helpers/OfferProcess.php
+++ app/Helpers/OfferProcess.php
@@ -2,20 +2,54 @@
 
 namespace App\Helpers;
 
-use Carbon\Carbon;
-use App\Order;
-use App\Helpers\AndroidNotifications;
-use App\User;
-use App\Http\Resources\Order\Order as OrderResource;
-use App\LifterLocation;
-use App\CartOrder;
-use Illuminate\Support\Facades\Cache;
 
 class OfferProcess {
 
-    static public function processOffer($code, $amount)
+    static public function processOffer($user, $code, $shopping)
     {
-
+        $offer = \App\Offer::where('promo_code', $code)->where('status',1)->first();
+            if($offer == null){
+                $data = [
+                    'msg' => 'Offer is expire.'
+                ];
+                return ['status'=>false, 'data' => $data];
+            }
+            $isAvail = \App\CartOrder::where('consumer_id', $user)->where(function($query) {
+                $query->where('status', 'droped')
+                      ->orWhere('status', 'confirmed');
+            })->where('coupon',$code)->count();
+            if($isAvail > 0){
+                $data = [
+                    'msg' => "You have already avail this offer."
+                ];
+                return ['status'=>false, 'data' => $data];
+            }
+            else if($shopping < $offer->shopping_limit){
+                $amount = $offer->shopping_limit - $shopping;
+                $data = [
+                    'msg' => "You have to shop more RS{$amount} to avail this offer."
+                ];
+                return ['status'=>false, 'data' => $data];
+            }else if($offer->credit == 1){
+                $data = [
+                    'msg' => "Congratulation! You can save upto {$offer->amount}{$offer->type} on {$offer->category}.",
+                ];
+                return ['status'=>true, 'data' => $data];
+            }else if($offer->credit == 0){
+                $amount = 0;
+                if($offer->type == '%'){
+                    $amount = round($shopping * $offer->amount / 100);
+                }else if($offer->type == 's'){
+                    $amount = $offer->amount;
+                }
+                $data = [
+                    'msg' => "Congratulation! You have to save {$offer->amount}{$offer->type} on {$offer->category}.",
+                    'amount' => $amount
+                ];
+                return ['status'=>true, 'data' => $data];
+            }
+            $data = ['msg' => 'Some thing not process'];
+            return ['status'=>false, 'data' => $data];
     }
         
 }
--- app/Http/Controllers/Api/Consumer/OfferController.php
+++ app/Http/Controllers/Api/Consumer/OfferController.php
@@ -21,52 +21,54 @@ class OfferController extends Controller
 
     }
 
-    public function update(Request $request, $id)
+    public function update(Request $request, $coupon)
     {
         try{
-            $offer = Offer::where('promo_code', $id)->where('status',1)->first();
-            if($offer == null){
-                $data = [
-                    'msg' => 'Offer is expire.'
-                ];
-                return response()->json(['status'=>false, 'data' => $data], 200);
-            }
-            $isAvail = \App\CartOrder::where('consumer_id', $request->user()->id)->where(function($query) {
-                $query->where('status', 'droped')
-                      ->orWhere('status', 'confirmed');
-            })->where('coupon',$id)->count();
-            if($isAvail > 0){
-                $data = [
-                    'msg' => "You have already avail this offer."
-                ];
-                return response()->json(['status'=>false, 'data' => $data], 200);
-            }
-            else if($request->amount < $offer->shopping_limit){
-                $amount = $offer->shopping_limit - $request->amount;
-                $data = [
-                    'msg' => "You have to shop more RS{$amount} to avail this offer."
-                ];
-                return response()->json(['status'=>false, 'data' => $data], 200);
-            }else if($offer->credit == 1){
-                $data = [
-                    'msg' => "Congratulation! You can save upto {$offer->amount}{$offer->type} on {$offer->category}.",
-                ];
-                return response()->json(['status'=>true, 'data' => $data], 200);
-            }else if($offer->credit == 0){
-                $amount = 0;
-                if($offer->type == '%'){
-                    $amount = round($request->amount * $offer->amount / 100);
-                }else if($offer->type == 's'){
-                    $amount = $offer->amount;
-                }
-                $data = [
-                    'msg' => "Congratulation! You have to save {$offer->amount}{$offer->type} on {$offer->category}.",
-                    'amount' => $amount
-                ];
-                return response()->json(['status'=>true, 'data' => $data], 200);
-            }
-            $data = ['msg' => 'Some thing not process'];
-            return response()->json(['status'=>true, 'data' => $data], 200);
+            $response = App\Helpers\OfferProcess::processOffer($request->user()->id, $coupon, $request->amount);
+            return response()->json( $response, 200);
+            // $offer = Offer::where('promo_code', $id)->where('status',1)->first();
+            // if($offer == null){
+            //     $data = [
+            //         'msg' => 'Offer is expire.'
+            //     ];
+            //     return response()->json(['status'=>false, 'data' => $data], 200);
+            // }
+            // $isAvail = \App\CartOrder::where('consumer_id', $request->user()->id)->where(function($query) {
+            //     $query->where('status', 'droped')
+            //           ->orWhere('status', 'confirmed');
+            // })->where('coupon',$id)->count();
+            // if($isAvail > 0){
+            //     $data = [
+            //         'msg' => "You have already avail this offer."
+            //     ];
+            //     return response()->json(['status'=>false, 'data' => $data], 200);
+            // }
+            // else if($request->amount < $offer->shopping_limit){
+            //     $amount = $offer->shopping_limit - $request->amount;
+            //     $data = [
+            //         'msg' => "You have to shop more RS{$amount} to avail this offer."
+            //     ];
+            //     return response()->json(['status'=>false, 'data' => $data], 200);
+            // }else if($offer->credit == 1){
+            //     $data = [
+            //         'msg' => "Congratulation! You can save upto {$offer->amount}{$offer->type} on {$offer->category}.",
+            //     ];
+            //     return response()->json(['status'=>true, 'data' => $data], 200);
+            // }else if($offer->credit == 0){
+            //     $amount = 0;
+            //     if($offer->type == '%'){
+            //         $amount = round($request->amount * $offer->amount / 100);
+            //     }else if($offer->type == 's'){
+            //         $amount = $offer->amount;
+            //     }
+            //     $data = [
+            //         'msg' => "Congratulation! You have to save {$offer->amount}{$offer->type} on {$offer->category}.",
+            //         'amount' => $amount
+            //     ];
+            //     return response()->json(['status'=>true, 'data' => $data], 200);
+            // }
+            // $data = ['msg' => 'Some thing not process'];
+            // return response()->json(['status'=>true, 'data' => $data], 200);
         }catch(Expection $ex){
             return response()->json(['status'=>false, 'data'=>"$ex"], 401);
         }
