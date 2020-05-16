6fd0f8ee8416327d4ef1071ab78d4cbe36649076
mianazeemdaula <mianazeemdaula@gmail.com>
2020-05-16 13:11:02 +0500

    cs

--- routes/web.php
+++ routes/web.php
@@ -125,6 +125,6 @@ Route::get('catProduct', function(){
 });
 
 Route::get('pass', function(){
-    $payable = \App\CartOrder::where('consumer_id', 7)->where('status','droped')->sum('payable_amount');
+    $payable = \App\CartOrder::where('consumer_id', 7)->where('status','droped')->select(\DB::raw("payable_amount-charges"));
     return $payable;
 });
