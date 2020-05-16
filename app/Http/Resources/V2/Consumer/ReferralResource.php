fa11015dc156898395fdd3a310afebb795e3d4a5
mianazeemdaula <mianazeemdaula@gmail.com>
2020-05-16 12:59:34 +0500

    payable

--- app/Http/Resources/V2/Consumer/ReferralResource.php
+++ app/Http/Resources/V2/Consumer/ReferralResource.php
@@ -15,14 +15,15 @@ class ReferralResource extends JsonResource
     public function toArray($request)
     {
         $recent = \App\CartOrder::where('consumer_id',$this->id)->where('status','droped')->latest()->first();
+        $payable = \App\CartOrder::where('consumer_id', $this->id)->where('status','droped')->sum('payable_amount');
         return [
             'id' => $this->id,
             'name' => $this->name,
             'dateOfSignup' => $this->created_at,
             'recentShopping' => $recent == null ? 0 : $recent->payable_amount,
-            'totalShopping' => 0,
+            'totalShopping' => $payable == null ? 0 : round($payable),
             'recentCommission' => $recent == null ? 0 : round((($recent->payable_amount - $recent->charges - $recent->consumer_bonus) * 2 ) / 100),
-            'totalCommission' => \App\CartOrder::where('consumer_id', $this->id)->where('status','droped')->sum('payable_amount'),
+            'totalCommission' => $payable == null ? 0 : round(($payable * 2) / 100),
             'expiry' => \Carbon\Carbon::parse($this->created_at)->addYears(1),
         ];
     }
