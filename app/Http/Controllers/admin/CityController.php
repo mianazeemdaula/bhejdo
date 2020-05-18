7a9318f182125deeb1bf5799ff8ab0dec07a28f2
mianazeemdaula <mianazeemdaula@gmail.com>
2020-05-18 09:49:56 +0500

    city

--- app/Http/Controllers/admin/CityController.php
+++ app/Http/Controllers/admin/CityController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use Kris\LaravelFormBuilder\FormBuilderTrait;
-use App\Forms\Admin\Category\CreateCityForm;
+use App\Forms\Admin\City\CreateCityForm;
 
 use App\City;
 
